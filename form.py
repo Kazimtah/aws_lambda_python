3c266f569883a0c71753edc482fc529b3d120b40
Kazimtha <kazimhamid02@gmail.com>
2025-12-15 17:13:56 +0100

    adding new features like abstraction

--- form.py
+++ form.py
@@ -24,6 +24,23 @@ class Forum:
         post = Post(title, content,author)
         self.posts.append(post)
         return post 
+    # find the user by the username
+    def find_post_username(self, username):
+        for username in self.users:
+            if self.usersname == username:
+                return username
+
+# defining function by emmail
+    def find_post_email(self, email):
+        for email in self.users:
+            if self.users == email:
+                return email 
+
+#find the by post
+    def find_post(self,author):
+        for autheor in self.author:
+            if autheor == self.author:
+                return autheor
 
 forum = Forum()
 first_form = forum.register_user("booba","boba@gmail.com")
@@ -31,3 +48,4 @@ second_form = forum.create_post("my title", "love", first_form)
 print(forum.users)
 print(forum.users[0].username)
 print(forum.posts[0].title)
+print(first_form.find_post("booba"))
