2ae79222754b02a0febcb2a8e5f2ae25874e93b0
Kazimtha <kazimhamid02@gmail.com>
2026-01-02 21:35:01 +0100

    adding enumerate into cvs file

--- csv1.py
+++ csv1.py
@@ -1,7 +1,7 @@
 import csv
 
 with open('test.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file, delimiter=';')
+    writer = csv.writer(csv_file)
     writer.writerow(['user_id','user_name',' comments_qtn'])
     writer.writerow([5434, 'bodan', 1300])
     writer.writerow([4499, 'alice', 830])
@@ -9,7 +9,13 @@ with open('test.csv', 'w') as csv_file:
 
 with open('test.csv','r') as csv_file:
     _csv_reader = csv.reader(csv_file)
-    for line in _csv_reader:
-        print(line)
-    print(_csv_reader)
-    print(type(_csv_reader))
+    #for line in _csv_reader:
+     #   print(line)
+    for index ,element in enumerate(_csv_reader):
+        print(index,element)
+        if index == 0:
+            header = element
+        else:
+           for index,value in enumerate(element):
+
+    
